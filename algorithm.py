f93a2499e757c4c810e0db38acd5b9a4adaa8f09
Max991021 <leshopep@gmail.com>
2026-02-08 20:48:18 +0200

    Added external file for people in the hackathon

--- algorithm.py
+++ algorithm.py
@@ -1,36 +1,42 @@
 import random 
+import sys
 
 def people():
-    listinput = input('Names of people in the mini hacks?: ')
-    people = [element for element in listinput.strip().split(',')]
     
-    count = 0
-    groupselection = {}
-    
-    while len(people)>1:
-        count += 1
+    with open('available_people.txt', 'r', errors='ignore') as file:
+        contents = file.read()
         
-        try:
-            if len(people)%2 == 0:
-                peeps = random.sample(people, k=2)
-                groupselection['Group: '+str(count)] = peeps
-                    
-                for ind,ele in enumerate(peeps):
-                    people.pop(people.index(ele))
-            else:
-                print(f"The group is missing one person to complete the algorithm, the count of people is {len(people)} with the names {people}")
-                break
-        except ValueError as e:
-            e = f"The group is missing one person to complete the algorithm, the count of people is {len(people)} with the names {people}"
-            print(e)
+        people = [element for element in contents.strip().split()]
+        print(people)
+        if len(people)==0:
+            print('No People listed')
+            sys.exit()
+        count = 0
+        groupselection = {}
+        
+        while len(people)>1:
+            count += 1
             
-    else:
-        print('No people chosen or list is less than 2 people')
-    
-    print(groupselection)
+            try:
+                if len(people)%2 == 0:
+                    peeps = random.sample(people, k=2)
+                    groupselection['Group: '+str(count)] = peeps
+                        
+                    for ind,ele in enumerate(peeps):
+                        people.pop(people.index(ele))
+                else:
+                    print(f"The group is missing one person to complete the algorithm, the count of people is {len(people)} with the names {people}")
+                    break
+            except ValueError as e:
+                e = f"The group is missing one person to complete the algorithm, the count of people is {len(people)} with the names {people}"
+                print(e)
+                
+        else:
+            print('No people chosen or list is less than 2 people')
+        
+        print(groupselection)
     
 people()
-            
-        
+         
     
     
